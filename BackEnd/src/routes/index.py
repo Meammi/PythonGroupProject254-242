59cb61a3bc9d595f345e3662ea840ee5bd993a1d
PawaritMangnimit <pawarit.man@dome.tu.ac.th>
2026-04-27 22:11:58 +0700

    Update index.py

--- BackEnd/src/routes/index.py
+++ BackEnd/src/routes/index.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from src.routes.health.health_routes import router as health_router
+from src.routes.facilities.facility_routes import router as facilities_router
 
 api_router = APIRouter()
 api_router.include_router(health_router)
+api_router.include_router(facilities_router)
